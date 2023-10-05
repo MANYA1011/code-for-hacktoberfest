d92851324700df2c151ec2bcb4e7af157728ee84
manya <93120998+MANYA1011@users.noreply.github.com>
2023-10-05 19:39:01 +0530

    Update Bubblesort.cpp

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -3,25 +3,25 @@ using namespace std;
 int main ()
 {
    int i, j,temp;
-   int a[5] = {10,2,0,43,12};
+   int arr[5] = {10,2,0,43,12};
    cout <<"Input list\n";
    for(i = 0; i<5; i++) {
-   cout <<a[i]<<"\t";
+   cout <<arr[i]<<"\t";
 }
 cout<<endl;
 for(i = 0; i<5; i++) {
    for(j = i+1; j<5; j++)
    {
-      if(a[j] < a[i]) {
-         temp = a[i];
-         a[i] = a[j];
-         a[j] = temp;
+      if(arr[j] < arr[i]) {
+         temp = arr[i];
+         arr[i] = arr[j];
+         arr[j] = temp;
       }
    }
 }
 cout <<"Sorted Element List\n";
 for(i = 0; i<5; i++) {
-   cout <<a[i]<<"\t";
+   cout <<arr[i]<<"\t";
    }
 return 0;
 }
